6770c5e9f8a131acacdf6df27fad02b98947c2d9
Jack Mason <mrsideshowjack@gmail.com>
2016-01-31 18:04:00 +0000

    added image carousel on blog posts

---
+++ scripts/mooreProps.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+//Array of images which you want to show: Use path you want.
+var images=new Array('img/back1.jpg','img/back2.jpg','img/back3.jpg','img/back4.jpg');
+var nextimage=0;
+doSlideshow();
+
+function doSlideshow(){
+    if(nextimage>=images.length){nextimage=0;}
+    $('body')
+    .css('background-image','url("'+images[nextimage++]+'")')
+    .fadeIn(500,function(){
+        setTimeout(doSlideshow,7000);   //time the image stays on screen
+    });
+}
+
+ 
+  $("#owl").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+  });
+ 
+});
